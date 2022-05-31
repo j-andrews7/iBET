240599498d91fb1057fcb2386280b9b65f0bea5b
Jared Andrews <jared.andrews07@gmail.com>
2022-05-31 10:10:16 -0500

    Fix rowname swap for DF.

--- R/data_utils.R
+++ R/data_utils.R
@@ -1,24 +1,24 @@
 #' @importFrom SummarizedExperiment rowData rowData<-
 .swap_rownames <- function(object, swap.rownames = NULL) {
-  
+
   if (!identical(swap.rownames, NULL) && is(object, "SummarizedExperiment")) {
-    
+
     if (!swap.rownames %in% names(SummarizedExperiment::rowData(object))) {
       stop("'swap.rownames' is not a column of 'rowData(object)'")
     }
-    rowData(object)$ORIGINAL_ROWS <- rownames(object) 
+    rowData(object)$ORIGINAL_ROWS <- rownames(object)
     rownames(object) <- rowData(object)[,swap.rownames]
   }
-  
+
   if (!identical(swap.rownames, NULL) && is(object, "data.frame")) {
-    
+
     if (!swap.rownames %in% names(object)) {
       stop("'swap.rownames' is not a column of 'object'")
     }
-    
+
     object$ORIGINAL_ROWS <- rownames(object)
-    rownames(object) <- object[[swap.rownames]]
+    rownames(object) <- make.names(object[[swap.rownames]], unique = TRUE)
   }
-  
+
   object
 }
