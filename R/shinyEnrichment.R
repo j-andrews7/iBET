d08ed6e9335064e3157afae0f3d1a203adb333a1
Jacob <jacob@martin-email.com>
2025-12-09 13:53:59 -0600

    Dot and Bar Ggplot adaptation

--- R/shinyEnrichment.R
+++ R/shinyEnrichment.R
@@ -13,7 +13,7 @@ library(ggtree)
 library(enrichplot)
 library(org.Hs.eg.db)
 library(GOSemSim)
-
+library(colourpicker)
 shinyEnrichment <- function(enrichmentObj) {
     ui <- dashboardPage(
         dashboardHeader(disable = TRUE),
@@ -28,7 +28,10 @@ shinyEnrichment <- function(enrichmentObj) {
                     selectInput("colour_by", "Colour by:", c("p.adjust", "pvalue", "qvalue")),
                     numericInput("num_categoriesDot", "Number of Gene Sets:", value = 10, min = 1, max = 50),
                     textInput("titleDotPlot", "Enter the title for the dot plot:"),
-                    selectInput("x_axisSelection", "X-axis:", c("Count", "GeneRatio"))
+                    selectInput("x_axisSelection", "X-axis:", c("Count", "GeneRatio")),
+                    numericInput("dot_textSize", "Y Axis Text Size: ", value = 10, min = 1, max = 50),
+                    colourInput("my_color_Dot1", "Select a color", value = "red"),
+                    colourInput("my_color_Dot2", "Select a color", value = "blue")
                 ),
                 # Bar Plot Button UI
                 bsCollapsePanel(
@@ -37,7 +40,10 @@ shinyEnrichment <- function(enrichmentObj) {
                     numericInput("num_categories_Bar", "Categories:", value = 20, min = 1, max = 50),
                     selectInput("colour_by_Bar", "Colour by:", c("p.adjust", "pvalue", "qvalue")),
                     textInput("titleBarPlot", "Enter the title for the dot plot:"),
-                    selectInput("x_axisSelectionBar", "X-axis:", c("Count", "GeneRatio"))
+                    selectInput("x_axisSelectionBar", "X-axis:", c("Count", "GeneRatio")),
+                    numericInput("bar_textSize", "Y Axis Text Size: ", value = 10, min = 1, max = 50),
+                    colourInput("my_color_Bar1", "Select a color", value = "red"),
+                    colourInput("my_color_Bar2", "Select a color", value = "blue")
                 ),
                 #Heat PLot
                 bsCollapsePanel(
@@ -114,26 +120,27 @@ shinyEnrichment <- function(enrichmentObj) {
     server <- function(input, output, session) {
         # Creating Dot Plot Output:
         # DotPlot Output:
-        enrich_with_termsim <- pairwise_termsim(enrichmentObj, 
-                                       method = "Wang", 
-                                       semData = godata("org.Hs.eg.db", ont = "BP"))
+        # enrich_with_termsim <- pairwise_termsim(enrichmentObj, 
+        #                                method = "Wang", 
+        #                                semData = godata("org.Hs.eg.db", ont = "BP"))
         output$dotplot <- renderPlotly(
-            dotPlotEnrichment(enrich = enrichmentObj, numSets = input$num_categoriesDot, colourBy = input$colour_by, titleForPlot = input$titleDotPlot, x_axis = input$x_axisSelection)
+            dotPlotEnrichment(enrich = enrichmentObj, numSets = input$num_categoriesDot, colourBy = input$colour_by, titleForPlot = input$titleDotPlot, x_axis = input$x_axisSelection, textSize = input$dot_textSize, colour1 = input$my_color_Dot1, colour2 = input$my_color_Dot2)
         )
 
         output$barPlot <- renderPlotly(
-            barPlotEnrichment(enrichObj = enrichmentObj, numSets = input$num_categories_Bar, colourBy = input$colour_by_Bar, titleForPlot = input$titleBarPlot, x_axis = input$x_axisSelectionBar)
+            barPlotEnrichment(enrich = enrichmentObj, numSets = input$num_categories_Bar, colourBy = input$colour_by_Bar, titleForPlot = input$titleBarPlot, x_axis = input$x_axisSelectionBar, colour1 = input$my_color_Bar1, colour2 = input$my_color_Bar2, textSize = input$bar_textSize)
         )
-        output$heatPlot <- renderPlotly(
-            heatPlotEnrichment(enrich = enrichmentObj, numSets = input$heat_categories)
-        )
-        output$treePlot <- renderPlot(
-            treePlotEnrichment(enrich = enrich_with_termsim, numSets = 20)
-        )
+        # output$heatPlot <- renderPlotly(
+        #     heatPlotEnrichment(enrich = enrichmentObj, numSets = input$heat_categories)
+        # )
+        # output$treePlot <- renderPlot(
+        #     treePlotEnrichment(enrich = enrich_with_termsim, numSets = 20)
+        # )
     }
 
     shinyApp(ui = ui, server = server, options = list(launch.browser = TRUE))
 }
 test <- readRDS("/Volumes/JM/Development/iBET/Play/ExampleEnrichment.rds")
+test <- as.data.frame(test)
 app <- shinyEnrichment(enrichmentObj = test)
 runApp(app)
--- R/shinyEnrichment_utils.R
+++ R/shinyEnrichment_utils.R
@@ -1,78 +1,122 @@
-
-#' @title DotPlot Function For Enrichment Results 
-#' @param enrich ClusterProfiler enrichment Object from enrichGO() function 
-#' @param numSets the number of gene sets you want to be displayed in the plot 
-#' @param colourBy Either p.adjust, qvalue or pvalue. Determines how the dots are coloured on a colour scale.
-#' @param titleForPlot Title for the plot
-#' @param x_axis Whether the x axis represents the GeneRatio or Count  
+library(shiny)
+library(shinydashboard)
+library(plotly)
+library(shinyBS)
+library(shinyjs)
+library(dashboardthemes)
+library(bslib)
+library(shinyjqui)
+library(clusterProfiler)
+library(ggtree)
+library(enrichplot)
+library(org.Hs.eg.db)
+library(GOSemSim)
+library(forcats)
+#' @title Dot Plot Enrichment Visualization
+#' @description Creates interactive dot plot for enrichment results
+#' @param enrich Enrichment result object (data.frame-like)
+#' @param numSets Number of top results to show (default: 10)
+#' @param colourBy Column name for color gradient (default: "p.adjust")
+#' @param titleForPlot Plot title
+#' @param x_axis X-axis variable ("GeneRatio" or "Count")
+#' @param textSize Y-axis text size
+#' @param colour1 Low end color
+#' @param colour2 High end color
+#' @return plotly object
+#' @importFrom rlang sym .data
+#' @importFrom ggplot2 ggplot aes geom_point scale_size_continuous theme_minimal element_text labs scale_color_gradient reorder
 #' @importFrom plotly ggplotly
-#' @importFrom clusterProfiler dotplot
-#' @importFrom ggplot2 ggplot
-#' @author Jacob Martin 
+#' @aurhtor Jacob Martin
 #' @export
-# Dot Plot customisation: 
-# Colour of the balls: p.adjust, pvalue or qvalue: 
-#showCategory 
-#Function for DotPlot graphing: 
-# Title 
-dotPlotEnrichment <- function(enrich, numSets = 10, colourBy = "p.adjust", titleForPlot = "Title", x_axis = "GeneRatio"){
-    dotPlot <- dotplot(enrich, x = x_axis, showCategory = numSets, color = colourBy, title = titleForPlot)
-    plotlyOut <- ggplotly(dotPlot, tooltip = c("text", "Count", "Description", colourBy))
+
+dotPlotEnrichment <- function(enrich, numSets = 10, colourBy = "p.adjust", titleForPlot = "Title", x_axis = "GeneRatio", textSize = 10, colour1 = "red", colour2 = "blue"){
+    enrich <- enrich[order(enrich$p.adjust), ]
+    enrich <- enrich[1:numSets, ]
+    dotPlot <- ggplot(enrich, aes(x = !!sym(x_axis), y = reorder(Description, !!sym(x_axis)), color = .data[[colourBy]], size = Count, group = Description, text = Description)) +
+        geom_point(alpha = 0.7) +
+        scale_size_continuous(range = c(3, 7), name = "Count") + 
+        theme_minimal() +
+        theme(
+            axis.text.y = element_text(color = "black", size = textSize, angle = 0, hjust = .5, vjust = .5, face = "plain"),
+            axis.text.x = element_text(color = "black", size = 10, angle = 0, hjust = .5, vjust = .5, face = "plain")
+        )+
+        labs(x = paste(x_axis, "(log10)"), y = "Description", title = titleForPlot)+
+        scale_color_gradient(low = colour1, high = colour2, name = colourBy)
+    plotlyOut <- ggplotly(dotPlot, tooltip = c("x", "y", "size", colourBy))
     return(plotlyOut)
 }
 
 
-#'@title BarPlot Function for enrichment Results
-#' @param enrichObj enrichGO object from cluster profiler
-#' @param numSets the number of gene sets you want to be displayed in the plot
-#' @param colourBy The colour gradient is determined by: pvalue, p.adjust or qvalue 
-#' @param titleForPlot Title for the plot
-#' @param x_axis Whether the x axis represents the GeneRatio or Count 
-#' @importFrom plotly ggplotly 
-#' @importFrom clusterProfiler barplot
-#' @importFrom ggplot2 ggplot
-#' @author Jacob Martin 
-#' @export
+# #'@title BarPlot Function for enrichment Results
+# #' @param enrichObj enrichGO object from cluster profiler
+# #' @param numSets the number of gene sets you want to be displayed in the plot
+# #' @param colourBy The colour gradient is determined by: pvalue, p.adjust or qvalue 
+# #' @param titleForPlot Title for the plot
+# #' @param x_axis Whether the x axis represents the GeneRatio or Count 
+# #' @importFrom plotly ggplotly 
+# #' @importFrom clusterProfiler barplot
+# #' @importFrom ggplot2 ggplot
+# #' @author Jacob Martin 
+# #' @export
 
-barPlotEnrichment <- function(enrichObj, numSets = 10, colourBy = "p.adjust", titleForPlot = "Title", x_axis = "GeneRatio"){
-    barPlot <- barplot(enrichObj, x = x_axis, showCategory = numSets, color = colourBy, title = titleForPlot, )
-    plotlyBarOut <- ggplotly(barPlot, tooltip = c("text", "Count", "GeneRatio", "Description", colourBy))
-    return(plotlyBarOut)
+barPlotEnrichment <- function(enrich, numSets = 10, colourBy = "p.adjust", titleForPlot = "Title", x_axis = "GeneRatio", textSize = 10, colour1 = "red", colour2 = "blue"){
+    
+    enrich_df <- as.data.frame(enrich)[order(as.data.frame(enrich)[[colourBy]]), ][1:numSets, ]
+    enrich_df[[x_axis]] <- as.numeric(as.character(enrich_df[[x_axis]]))
+    barPlot <- ggplot(enrich_df, aes(x = reorder(Description, !!sym(x_axis)), y = !!sym(x_axis), fill = .data[[colourBy]], text = Description)) +
+        geom_col(alpha = 0.8, width = 0.7) +
+        scale_fill_gradient(low = colour1, high = colour2, name = colourBy) +
+        labs(x = "Description", y = paste(x_axis, "(log10)"), title = titleForPlot) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(color = "black", size = textSize, angle = 45, hjust = 1),
+              axis.text.y = element_text(color = "black", size = textSize)) +
+        coord_flip()  # Horizontal bars for long descriptions
+    
+    plotlyOut <- ggplotly(barPlot, tooltip = c("text", "y", "fill"))
+    return(plotlyOut)
 }
 
-#'@title HeatMap Plot
-#' @param enrich enrichGO object from cluster profiler
-#' @param numSets the number of gene sets you want to be displayed in the plot
-#' @importFrom plotly ggplotly 
-#' @importFrom clusterProfiler heatplot
-#' @importFrom ggplot2 ggplot
-#' @author Jacob Martin 
-#' @export
-#' 
-heatPlotEnrichment <- function(enrich, numSets = 10){
-    heatPlot <- heatplot(enrich, showCategory = numSets)
-    plotlyHeatOut <- ggplotly(heatPlot, tooltip = c("text", "Count", "GeneRatio", "Description"))
-    return(plotlyHeatOut)
-}
+# #'@title HeatMap Plot
+# #' @param enrich enrichGO object from cluster profiler
+# #' @param numSets the number of gene sets you want to be displayed in the plot
+# #' @importFrom plotly ggplotly 
+# #' @importFrom clusterProfiler heatplot
+# #' @importFrom ggplot2 ggplot
+# #' @author Jacob Martin 
+# #' @export
+# #' 
+# heatPlotEnrichment <- function(enrich, numSets = 10){
+#     heatPlot <- heatplot(enrich, showCategory = numSets)
+#     plotlyHeatOut <- ggplotly(heatPlot, tooltip = c("text", "Count", "GeneRatio", "Description"))
+#     return(plotlyHeatOut)
+# }
 
-#'@title Tree Plot
-#' @param enrich enrichGO object from cluster profiler
-#' @param numSets the number of gene sets you want to be displayed in the plot
-#' @param colourBy The colour gradient is determined by: pvalue, p.adjust or qvalue 
-#' @importFrom plotly ggplotly 
-#' @importFrom enrichplot treeplot
-#' @importFrom ggplot2 ggplot
-#' @author Jacob Martin 
-#' @export
-library(enrichplot)
-treePlotEnrichment <- function(enrich, numSets = 10, colourBy = "p.adjust"){
-    treePlot <- treeplot(enrich, showCategory = numSets, color = colourBy)
-    plotlyHeatOut <- ggplotly(treePlot)
-    return(plotlyHeatOut)
-}
+# #'@title Tree Plot
+# #' @param enrich enrichGO object from cluster profiler
+# #' @param numSets the number of gene sets you want to be displayed in the plot
+# #' @param colourBy The colour gradient is determined by: pvalue, p.adjust or qvalue 
+# #' @importFrom plotly ggplotly 
+# #' @importFrom enrichplot treeplot
+# #' @importFrom ggplot2 ggplot
+# #' @author Jacob Martin 
+# #' @export
+# treePlotEnrichment <- function(enrich, numSets = 10, colourBy = "p.adjust"){
+#     treePlot <- treeplot(enrich, showCategory = numSets, color = colourBy)
+#     plotlyHeatOut <- ggplotly(treePlot)
+#     return(plotlyHeatOut)
+# }
+
+
+
+
+# dotPlotEnrichmentCustom <- ggplot(aes(x = enrich$))
+#Description: 
+#pvalue 
+#p.adjust 
+#GeneRatio 
+#Count
 test <- readRDS("/Volumes/JM/Development/iBET/Play/ExampleEnrichment.rds")
-# # Before calling treePlotEnrichment()
-# # test <- simplify(test, cutoff = 0.7, by = "p.adjust", select_fun = min)  # Populates @termsim
-treePlotEnrichment(test, numSets = 10)
-
+test1 <- as.data.frame(test)
+x <- dotPlotEnrichment(enrich = test, numSets = 10, colourBy = "p.adjust", titleForPlot = "Title", x_axis = "GeneRatio", textSize = 10, colour1 = "red", colour2 = "blue")
+y <- barPlotEnrichment(enrich = test, numSets = 10, colourBy = "p.adjust", titleForPlot = "Title", x_axis = "GeneRatio", textSize = 10, colour1 = "red", colour2 = "blue")
 
